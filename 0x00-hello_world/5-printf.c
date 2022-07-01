259f7f1cfc9a9894a63bc686d312085fdf9bd3a4
aminaminumar <aminaminumar@gmail.com>
2022-07-01 05:58:55 -0700

    task2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main 
+ *
+ * Description: Return 0 after printing the function
+ */
+int main(void)
+{
+printf("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+~
+
