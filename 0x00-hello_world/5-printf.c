702a750f42c5fae4f649d44e0666d7d8eb9c8bdd
aminaminumar <aminaminumar@gmail.com>
2022-07-02 07:17:35 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * Description: main - Prints out a string to stout 
- * Return:0 if success
+ *Description: main - prints out a string to stout 
+ *Return:0 if success
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
 ~
