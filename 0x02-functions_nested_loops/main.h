8594ac495b5b3712fa46512173448b27f13f128d
aminaminumar <aminaminumar@gmail.com>
2022-07-06 06:05:36 -0700

    task0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * print_alphabet - prints alphabet and returns a new line
+ * print_alphabet_x10 - print alphabet 10 times
+ * _islower - return 1 if c is lowercase, and 0 otherwise
+ * _isalpha - return 1 if c is a letter, upper or lowercase, 0 otherwise
+ * print_sign - return 1 for +, 0 for 0, -1  for -
+ * _abs - computes absolute value of integer
+ * print_last_digit - prints last digit
+ * jack_bauer - prints all the minutes of his day
+ * times_table - prints times table up to 9
+ * add - add two ints, return sum
+ * print_to_98 - prints all natural numbers to 98
+ * print_times_table - prints table table up to 15
+ */
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
