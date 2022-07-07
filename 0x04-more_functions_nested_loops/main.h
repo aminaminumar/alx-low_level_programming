1fe301b0082861c33c948cb76f7cfb89961f370b
aminaminumar <aminaminumar@gmail.com>
2022-07-07 12:25:51 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * _isdigit - checks if int is a digit between 0-9
+ * mul - multiply two numbers
+ * print_void - prints numbers 0 - 9
+ * print_most_numbers - prints 0 - 9 without 2 and 4
+ * more_numbers - print 0 - 14 ten times
+ * print_line - print a line of n length
+ * print_diagonal - print a diagonal line of n length
+ * print_square - print a square of n size
+ * print_triangle - print a triangle of n size
+ */
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
